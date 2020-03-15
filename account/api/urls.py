af6016e8c0cfae9d1bb0efd743ecf445ace4f6f1
Cypherspark <persianrobo4@gmail.com>
2020-03-16 01:58:02 +0330

    initial signup

--- account/api/urls.py
+++ account/api/urls.py
@@ -1,16 +1,16 @@
-rom django.conf.urls import url
+from django.conf.urls import url
 
 
 from . import views
 
 urlpatterns = [
 
-    url(r'^login/$',
-        views.UserLoginAPIView.as_view(),
-        name='login'),
+    # url(r'^login/$',
+    #     views.LoginView.as_view(),
+    #     name='login'),
 
     url(r'^register/$',
-        views.UserRegistrationAPIView.as_view(),
+        views.SignupView.as_view(),
         name='register')
 
 ]
--- account/api/views.py
+++ account/api/views.py
@@ -1,19 +1,39 @@
-rom django.contrib.auth import get_user_model
-from django.contrib.sites.shortcuts import get_current_site
+
 from rest_framework import generics, permissions, status, views
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from account.models import User
-from . import serializers
+from account.api.serializers import *
+
+class SignupView(APIView):
+
+    def post(self, request):
+        serializer = UserSignupSerializer(data=request.data)
+        if serializer.is_valid():
+            u = serializer.save()
+            print(u)
+            return Response({
+                'message': 'your account have been created successfuly',
+                'data': serializer.data
+            })
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
 
 
 
 
-class UserRegistrationAPIView(generics.CreateAPIView):
-    """
-    Endpoint for user registration.
-    """
-    permission_classes = (permissions.AllowAny, )
-    serializer_class = serializers.UserRegistrationSerializer
-    queryset = User.objects.all()
+
+
+# class UserRegistrationAPIView(generics.CreateAPIView):
+#     """
+#     Endpoint for user registration.
+#     """
+#     permission_classes = (permissions.AllowAny, )
+#     serializer_class = serializers.UserRegistrationSerializer
+#     queryset = User.objects.all()
+# ``
