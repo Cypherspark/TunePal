4d0b3dad7d508aebc2e1eeec11c99bfd61aeaec1
Cypherspark <persianrobo4@gmail.com>
2020-03-30 02:00:30 +0430

    location registered in admin

--- account/admin.py
+++ account/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from .models import *
 from rest_framework.authtoken.admin import TokenAdmin
 
+
 TokenAdmin.raw_id_fields = ['user']
 admin.site.register(CustomUser)
+admin.site.register(UserLocation)
+
