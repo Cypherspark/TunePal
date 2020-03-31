3b4ad039a1e6a07c7e6593bacc4711a67d05fc90
reza <rezamansourikhah@gmail.com>
2020-03-31 12:24:15 +0430

    make a Match class to get access key and if true call function friends and function friends run from music/views.py then output is jason that see a list of friends

--- music/api/views.py
+++ music/api/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse
 from account.models import CustomUser
 from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+from django.http import JsonResponse
 
 
 import os
@@ -14,18 +16,21 @@ import spotipy.util as util
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 
+from music.api.serializers import UserProfileSerializer
 
+from music.views import friends
 
 # os.environ['SPOTIPY_CLIENT_ID'] = cid
 # os.environ['SPOTIPY_CLIENT_SECRET'] = secret
 # os.environ['SPOTIPY_REDIRECT_URI'] = 'http://127.0.0.1:8000/spotify/auth/'
 
 
-SPOTIPY_CLIENT_ID = 'b086b316d65241c2b7a1ac4e2e797c91'
-SPOTIPY_CLIENT_SECRET = '36cbb32cfe8c4ecbba50083ce277b1ab'
+SPOTIPY_CLIENT_ID = '8167c1d1f43643e989ae8f5b427205c7'
+SPOTIPY_CLIENT_SECRET = '2b506c6b41da420c80dc242916f9ac69'
 SPOTIPY_REDIRECT_URI = 'http://127.0.0.1:8000/spotify/auth/'
-SCOPE = 'user-top-read'
+SCOPE = 'user-top-read user-read-currently-playing user-read-playback-state user-library-read'
 CACHE = '.spotipyoauthcache'
 
 
@@ -39,12 +44,11 @@ def getSPOauthURI():
 
 
 class SpotifyView(APIView):
-
     @csrf_exempt
     def get(self, request):
         if request.user.is_authenticated :
             access_token = ""
-            
+
             token_info = sp_oauth.get_cached_token(request)
 
             if token_info:
@@ -68,24 +72,69 @@ class SpotifyView(APIView):
                 return getSPOauthURI()
         else:
             # print("here")
-            return  Response( { 
+            return  Response( {
                'message': 'Your are not logged in'
             },
                 status=status.HTTP_400_BAD_REQUEST
             )
-   
 
-        # return Response(
-        #    {
-        #                 'message': 'Your are connected successfuly',
-        #                 'data': {
-        #                     'list_of_artist_names': list_of_artist_names,
-        #                 }
-        #             },
-        #             status=status.HTTP_200_OK
-        #         )
+class Match(APIView):
+    @csrf_exempt
+    def get(self, request):
+        if request.user.is_authenticated :
+            access_token = ""
 
-    # @api_view(['GET'])    
-    # def respose_to_auth(self, request, userparameter=None):
+            token_info = sp_oauth.get_cached_token(request)
+
+            if token_info:
+                print ("Found cached token!")
+                access_token = token_info['access_token']
+            else:
+                url = request.build_absolute_uri()
+                code = sp_oauth.parse_response_code(url)
+                if code:
+                    print ("Found Spotify auth code in Request URL! Trying to get valid access token...")
+                    token_info = sp_oauth.get_access_token(code,request=request)
+                    access_token = token_info['access_token']
+
+            if access_token:
+                print ("Access token available! Trying to get user information...")
+                sp = spotipy.Spotify(access_token)
+                friends(request,request.user.id,sp)
+                data = {}
+                for friend in friends.friends:
+                    a=1
+                    data = [{ a : friend}]
+                    a+=1
+                return JsonResponse(data, safe=False)
 
 
+            else:
+                return getSPOauthURI()
+        else:
+            # print("here")
+            return  Response( {
+               'message': 'Your are not logged in'
+            },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+
+
+
+class UserViewSet(viewsets.ViewSet):
+    """
+    A simple ViewSet that for listing or retrieving users.
+    """
+    def list(self, request):
+        queryset = CustomUser.objects.all()
+        serializer = UserProfileSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = CustomUser.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = UserProfileSerializer(user)
+        return Response(serializer.data)
+        user_detail = UserViewSet.as_view({'get': 'retrieve'})
