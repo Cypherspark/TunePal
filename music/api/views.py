cf33f2cc102b0594a48aca84b65c42a701d8004f
Masoud Golestaneh <52936110+Cypherspark@users.noreply.github.com>
2020-03-31 07:20:00 +0430

    Update views.py

--- music/api/views.py
+++ music/api/views.py
@@ -24,7 +24,7 @@ from rest_framework.permissions import IsAuthenticated
 
 SPOTIPY_CLIENT_ID = 'c42e107d3ae641e4af9e08e7d7a55b9b'
 SPOTIPY_CLIENT_SECRET = 'cd1e4e0aa3684e34ae12b313ebea1074'
-SPOTIPY_REDIRECT_URI = 'http://localhost:3000/spotifyresult'
+SPOTIPY_REDIRECT_URI = 'http://localhost:3000/spotifyresult/'
 SCOPE = 'user-top-read'
 CACHE = '.spotipyoauthcache'
 
