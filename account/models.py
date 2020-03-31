3198235306dd9fb8954c31a95c3da6055890b5f7
reza <rezamansourikhah@gmail.com>
2020-03-31 12:40:11 +0430

    add two calss friends that give just a username and a class music to save user favorite music_album ,music_genre,music_artist and music_name and assign tow models to customuser the relaton with friends is many to many and with music is one to many but i want change music model to many to many becuase this not good two make a music object for any user separate maybe make many same music that just fill database

--- account/models.py
+++ account/models.py
@@ -4,17 +4,26 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext_lazy as _
 
+class Music(models.Model):
+    music_name = models.CharField(max_length=250)
+    artist_name = models.CharField(max_length=250)
+    genre = models.CharField(max_length=250)
+    album = models.CharField(max_length=250)
+# class Music(models.Model):
+class Friends(models.Model):
+    username = models.CharField(_('username'), max_length=30, blank=True, unique=False)
+
 class CustomUser(AbstractUser):
     first_name = None
     last_name = None
     FEMALE = 'Female'
     MALE = 'Male'
-    
+
     TYPE_CHOICES = (
         (MALE, 'Male'),
         (FEMALE, 'Female'),
     )
-    
+
     gender = models.CharField(
         max_length=10,
         choices=TYPE_CHOICES,
@@ -27,6 +36,10 @@ class CustomUser(AbstractUser):
     intersts = models.CharField(_('interests'), max_length=30, blank=True, null=True)
     user_avatar = models.ImageField(upload_to="images/", blank=True)
     spotify_token = models.CharField(_('spotify token'), max_length=220, blank=True, null=True)
+    assigned = models.ForeignKey(Music, default=None, null=True,blank=True, on_delete=models.SET_NULL)
+    friends = models.ManyToManyField(Friends)
+
+
     #location =
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['gender', 'email','password']
