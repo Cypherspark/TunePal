2a16aa823f594d3f509871ca75b506bed5dce3c5
reza <rezamansourikhah@gmail.com>
2020-03-31 12:46:07 +0430

    add a url match and make this direct http://127.0.0.1:8000/spotify/match/ to start matching operation

--- music/api/urls.py
+++ music/api/urls.py
@@ -6,9 +6,15 @@ urlpatterns = [
     url(r'^auth/$',
        views.SpotifyView.as_view(),
         name='authorized'),
-
+    url(r'^profile/(?P<pk>\d+)$',
+            views. UserViewSet.as_view({'get': 'retrieve'}),
+            name='profile'),
+    url(r'^match/$',
+       views.Match.as_view(),
+        name='Matching'),
     # url(r'^/auth/$',
     #     views.SpotifyView.as_view(),
     #     name='spotify')
+
 
 ]
