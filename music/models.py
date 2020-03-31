4109b5ece9cd67549c68acd27b473dbce028178b
reza <rezamansourikhah@gmail.com>
2020-03-31 18:43:21 +0430

    add class user_top_music to store 5 of user top music and make many to many relation eith customuser in account model

--- music/models.py
+++ music/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class User_top_music(models.Model):
+    music_name = models.CharField(max_length=250)
+    artist_name = models.CharField(max_length=250)
+    genre = models.CharField(max_length=250)
+    album = models.CharField(max_length=250)
+    music_id = models.CharField(max_length=250)
