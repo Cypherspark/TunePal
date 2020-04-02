3ad38a5691350512eae9179280a1a56324022887
reza <rezamansourikhah@gmail.com>
2020-04-02 21:01:34 +0430

    remove some urls

--- account/api/urls.py
+++ account/api/urls.py
@@ -12,10 +12,5 @@ urlpatterns = [
     url(r'^sign_up/$',
         views.SignupView.as_view(),
         name='register'),
-    url(r'^profile$',
-            views. UserProfile.as_view({'get': 'retrieve'}),
-            name='profile'),
-    url(r'^update$',
-            views. UpdateUser.as_view(),
-            name='update'),
+
 ]
