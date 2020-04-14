1d0895620a0ba283970187273d9182df0c37a4d8
Cypherspark <persianrobo4@gmail.com>
2020-04-14 23:27:07 +0430

    fix makemigrations bugs

--- music/api/serializers.py
+++ music/api/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
 from account.models import CustomUser as User
-from account.models import Friends
+from account.models import Friend
 from TunePal import settings
 
 
-class FriendInfoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Friends
-        fields = ["username","nickname"]
+# class FriendInfoSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Friend
+#         fields = ["username","nickname"]
