bc3803f2d0e30dc7f9d063ca0234c33f2d913c7a
reza <rezamansourikhah@gmail.com>
2020-04-16 20:25:07 +0430

    add and remove some class

--- music/api/views.py
+++ music/api/views.py
@@ -56,7 +56,6 @@ class SpotifyGetTokenView(APIView):
         access_token = ""
 
         token_info = sp_oauth.get_cached_token(request)
-        print("sdoubgjsl")
         if token_info:
             print ("Found cached token!fildshcilk")
             access_token = token_info['access_token']
@@ -74,185 +73,53 @@ class SpotifyGetTokenView(APIView):
             results = sp.current_user()
             return Response({"url":"http://localhost:3000/spotifyresult/"})
 
-class Find_friends(APIView):
-    @csrf_exempt
-    @permission_classes([IsAuthenticated])
-    def get(self, request):
 
-        access_token = ""
-
-        token_info = sp_oauth.get_cached_token(request)
-
-        print ("Found cached token!")
-        access_token = token_info['access_token']
-
-        if access_token:
-            print ("Access token available! Wating for find your friends...")
-            sp = spotipy.Spotify(access_token)
-            friends(request,request.user.id,sp)
-            total_dict = {}
-            total_dict["FEMALE"] = friends.dataFE
-            total_dict["MALE"] = friends.dataM
-            return JsonResponse(total_dict, safe=False)
-
-
-
-class Match(APIView):
-    @csrf_exempt
-    @permission_classes([IsAuthenticated])
-    def get(self, request):
-        dataM = []
-        dataFE = []
-        activeuser = CustomUser.objects.get(id = request.user.id)
-        print(activeuser.id)
-        users = activeuser.friends.all()
-        for friend in users:
-            friendserialized = FriendInfoSerializer(friend)
-            dataM.append(friendserialized.data)
-
-
-        total_dict = {}
-        total_dict["FEMALE"] = dataFE
-        total_dict["MALE"] = dataM
-        return JsonResponse(total_dict, safe=False)
-class UserUpdateScore(GenericAPIView,UpdateModelMixin):
-    queryset = CustomUser.objects.all()
-    serializer_class = UserProfileUpdateScore
-    def put(self, request):
-        print(request.user.id)
-        question = get_object_or_404(CustomUser, pk=request.user.id)
-        serializer = UserProfileUpdateScore(question, data=request.data, partial=True)
-        if serializer.is_valid():
-            print("dsuogdsiuohiodshfo")
-            question = serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    def get(self,request):
-             if request.method == 'GET':
-                 queryset = CustomUser.objects.all()
-                 serializer = UserProfileUpdateScore(queryset, many=True)
-                 return Response(serializer.data)
 class User_Top_Music(GenericAPIView, UpdateModelMixin):
     queryset = CustomUser.objects.all()
-    def get(self, request):
-        list= []
-        user = get_object_or_404(self.queryset, pk=self.request.user.id)
-        songs = user.music.all()
-        for song in songs:
-            serializer = UserTopSongserialize(song)
-            list.append(serializer.data)
-
-        return Response(list)
-
-class question(APIView):
     @csrf_exempt
+    @permission_classes([IsAuthenticated])
     def get(self, request):
-                access_token = ""
+            access_token = ""
 
-                token_info = sp_oauth.get_cached_token(request)
+            token_info = sp_oauth.get_cached_token(request)
 
-                print ("Found cached token!")
-                access_token = token_info['access_token']
+            print ("Found cached token!")
+            access_token = token_info['access_token']
 
-                if access_token:
-                    print ("Access token available! Wating for find your friends...")
-                    sp = spotipy.Spotify(access_token)
-                    results = sp.current_user_top_tracks(
-                    limit=10, offset=0)
-                    for song in range(50):
-                        list = []
-                        list.append(results)
-                        with open('top50_data.json', 'w', encoding='utf-8') as f:
-                            json.dump(list, f, ensure_ascii=False, indent=4)
+            if access_token:
+                user = get_object_or_404(self.queryset, pk=self.request.user.id)
+                print ("Access token available! Wating for find your friends...")
+                sp = spotipy.Spotify(access_token)
+                results = sp.current_user_top_tracks(
+                limit=50, offset=0)
+                for song in range(50):
+                    list = []
+                    list.append(results)
+                    with open('top50_data.json', 'w', encoding='utf-8') as f:
+                        json.dump(list, f, ensure_ascii=False, indent=4)
 
-                    with open('top50_data.json') as f:
-                        data = json.load(f)
-                    list_of_results = data[0]["items"]
-                    list_of_artist_names=[]
-                    for result in list_of_results:
-                            result["album"]
-                            list_of_artist_names.append( result["artists"][0]["name"])
-                    temp = []
-                    for x in list_of_artist_names:
-                        if x not in temp:
-                            temp.append(x)
+                with open('top50_data.json') as f:
+                    data = json.load(f)
+                list_of_results = data[0]["items"]
 
-                    for result in list_of_results:
-                            result["album"]
-                            if result["artists"][0]["name"] in temp:
-                                temp.remove(result["artists"][0]["name"])
+                list_of_artist_names = []
+                list_of_song_names = []
+                list_of_albums = []
 
-                            print(temp)
-                            question = "the song with name "+result["name"]+" is for ...."
-                            list = random.sample(temp, len(temp))
-                            choices2 = list[0]
-                            choices3 = list[1]
-                            choices4 = list[2]
-                            list.append(result["artists"][0]["name"])
-                            answer = result["artists"][0]["name"]
-                            choices = random.sample(list,4)
-                            temp.append(result["artists"][0]["name"])
-                            # print(choices)
-                            Quiz1 = Quiz.objects.create(question = question,choices1 = choices[0],choices2 = choices[1] , choices3 = choices[2],choices4 = choices[3] , answer = answer)
-                            Activeuser = get_object_or_404(CustomUser,id =request.user.id)
-                            Activeuser.quiz.add(Quiz1)
+                for result in list_of_results:
+                    result["album"]
+                    list_of_artist_names.append( result["artists"][0]["name"])
+                    list_of_song_names.append(result["name"])
+                    list_of_albums.append(result["album"]["name"])
+                    music = User_top_music.objects.create(music_name = result["name"],artist_name =result["artists"][0]["name"],album = result["album"]["name"])
+                    user.music.add(music)
+                    user.save()
+                    for mn,al,ar in User_top_music.objects.values_list('music_name','album','artist_name').distinct():
+                                User_top_music.objects.filter(pk__in=User_top_music.objects.filter(music_name=mn,artist_name = ar,album = al).values_list('id', flat=True)[1:]).delete()
+            list= []
+            songs = user.music.all()
+            for song in songs:
+                serializer = UserTopSongserialize(song)
+                list.append(serializer.data)
 
-                    results = sp.current_user_top_artists(limit=5, offset=0, time_range='medium_term')
-                    for i in range(len(results)):
-                        temp.append(results['items'][i]['name'])
-                    for i in range(len(results)):
-
-                            if results['items'][i]['name'] in temp:
-                                temp.remove(results['items'][i]['name'])
-                            question = results['items'][i]['images'][2]['url']
-                            print(temp)
-                            list = random.sample(temp, 3)
-                            list.append(results['items'][i]['name'])
-                            answer = results['items'][i]['name']
-                            choices = random.sample(list,4)
-                            temp.append(results['items'][i]['name'])
-                            Quiz1 = Quiz.objects.create(question = question,choices1 = choices[0],choices2 = choices[1] , choices3 = choices[2],choices4 = choices[3] , answer = answer)
-                            Activeuser = get_object_or_404(CustomUser,id =request.user.id)
-                            Activeuser.quiz.add(Quiz1)
-                            for quiz in Quiz.objects.values_list('question', flat=True).distinct():
-                                        Quiz.objects.filter(pk__in=Quiz.objects.filter(question=quiz).values_list('id', flat=True)[1:]).delete()
-
-                    return Response(results)
-
-
-                else:
-                        return  Response( {
-                           'message': 'Your are not logged in'
-                        },
-                            status=status.HTTP_400_BAD_REQUEST
-                        )
-class privatequiz(GenericAPIView):
-    queryset = CustomUser.objects.all()
-    def get(self, request):
-        list= []
-        user = get_object_or_404(self.queryset, pk=self.request.user.id)
-        songs = user.quiz.all()
-        for song in songs:
-            serializer = Userprivatequiz(song)
-            list.append(serializer.data)
-        return Response(list)
-class checkanswer(GenericAPIView):
-     queryset = Quiz.objects.all()
-     serializer_class = Checkserialiser
-     def post(self,request):
-        quiz = get_object_or_404(self.queryset,id=request.POST.get('quiz_id'))
-        answer = request.POST.get('answer')
-        if quiz.answer == answer:
-            return Response('true')
-        else:
-            return Response("False")
-class publicquiz(GenericAPIView):
-    queryset = Quiz.objects.all()
-    def get(self,request):
-        questions = []
-        randomlist = random.sample(range(0,self.queryset.count()), 10)
-        for i in randomlist:
-            question = get_object_or_404(self.queryset, pk= i )
-            serializer = Userprivatequiz(question)
-            questions.append(serializer.data)
-        return Response(questions)
+            return Response(list)
