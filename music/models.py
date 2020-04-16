30cf96516955a1f5ad3f0bb6ca562630856f78ff
reza <rezamansourikhah@gmail.com>
2020-04-16 20:25:40 +0430

    remove a class

--- music/models.py
+++ music/models.py
@@ -14,11 +14,3 @@ class Music(models.Model):
     artist_name = models.CharField(max_length=250)
     genre = models.CharField(max_length=250)
     album = models.CharField(max_length=250)
-class Quiz(models.Model):
-    question = models.CharField(max_length=500)
-    choices1 = models.CharField(max_length = 20)
-    choices2 = models.CharField(max_length = 20)
-    choices3 = models.CharField(max_length = 20)
-    choices4 = models.CharField(max_length = 20, default = None)
-    answer = models.CharField(max_length = 20)
-    quiz_id = models.CharField(max_length = 200000000000000000000,blank=True,null = True,default = None)
