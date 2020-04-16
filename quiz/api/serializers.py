07345ceff3287f07ea5a75550359a5cce3e7217b
reza <rezamansourikhah@gmail.com>
2020-04-16 20:34:43 +0430

    add view and serializer class and add urls

---
+++ quiz/api/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from account.models import CustomUser as User
+from quiz.models import  QuizPassage,QuizImage
+
+class UserScore(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['score']
+class Imagequizserializer(serializers.ModelSerializer):
+    class Meta:
+        model = QuizPassage
+        fields = ["question",'choices1','choices2','choices3','choices4']
+class passagequizserializer(serializers.ModelSerializer):
+    class Meta:
+        model = QuizImage
+        fields = ["question",'choices1','choices2','choices3','choices4']
+class Checkimagequiz(serializers.ModelSerializer):
+            class Meta:
+                model = QuizPassage
+                fields = ["quiz_id",'answer']
+class Checkpassagequiz(serializers.ModelSerializer):
+            class Meta:
+                model = QuizImage
+                fields = ["quiz_id",'answer']
