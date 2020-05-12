674d97adcdc948a2315ffede04f005682d5750cf
Cypherspark <persianrobo4@gmail.com>
2020-05-13 03:20:53 +0430

    add core app

--- TunePal/urls.py
+++ TunePal/urls.py
@@ -24,6 +24,7 @@ from drf_yasg import openapi
 
 from django.conf import settings
 from django.conf.urls.static import static
+from core.views import index
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -39,7 +40,10 @@ schema_view = get_schema_view(
 )
 
 
+
+
 urlpatterns = [
+    path("", index, name="index"),
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
