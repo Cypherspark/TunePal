100043c92443827764ff9d368486deff6ec58ecc
Cypherspark <persianrobo4@gmail.com>
2020-05-13 19:36:30 +0430

    debug headers 3

--- chat/consumers.py
+++ chat/consumers.py
@@ -17,7 +17,7 @@ class ChatConsumer(WebsocketConsumer):
     def connect(self):
         
         self.user = self.scope["user"]
-        print(self.user,self.scope["header"])
+        print(self.user,self.scope["headers"])
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
