07e748aefbc518b23677c2312fb3ebcfd7727cdf
Masoud Golestaneh <52936110+Cypherspark@users.noreply.github.com>
2020-05-16 02:10:59 +0430

    Feature/websbsocket (#29)
    * add websocket chat
    
    * add a router
    
    * add websocket router
    
    * add token auth  to websocket
    
    * debug socket user
    
    * debug socket scope
    
    * debug socket header
    
    * debug headers 3
    
    * add token middleware stack
    
    Co-authored-by: reza <rezamansourikhah@gmail.com>
    Co-authored-by: Cypherspark <persianrobo4@gmail.com>

---
+++ TunePal/routing.py
@@ -0,0 +1,16 @@
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.http import AsgiHandler
+
+from TunePal.token_auth import TokenAuthMiddlewareStack
+import chat.routing
+
+application = ProtocolTypeRouter({
+    # (http->django views is added by default)
+    'websocket': TokenAuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
