74f41c291235e1c51d8bb4d6114f3e7bf0b605c0
Your Name <you@example.com>
2020-05-16 21:03:05 +0430

    debug print added

--- chat/consumers.py
+++ chat/consumers.py
@@ -17,7 +17,7 @@ class ChatConsumer(WebsocketConsumer):
     def connect(self):
         
         self.user = self.scope["user"]
-        # print(self.user,self.scope["headers"])
+        print("-------> i'm ",self.user)
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
