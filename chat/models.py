65108200a6bc7f47faba1b80ff3b853f24f2dde9
reza <rezamansourikhah@gmail.com>
2020-05-17 04:24:44 +0430

    add pin_massege field to conversation

--- chat/models.py
+++ chat/models.py
@@ -9,6 +9,10 @@ class Conversation(models.Model):
     name = models.CharField(max_length=100,blank=True,null =True)
     members = models.ManyToManyField(User)
     is_group = models.BooleanField(_("is_group"),default=False)
+    admin = models.ManyToManyField(User,blank = True, related_name = "admin")
+    pin_message = models.TextField()
+
+
 
     def __str__(self):
         return f"{self.id}"
@@ -28,7 +32,36 @@ class Message(models.Model):
 
     def __str__(self):
         return "%s (%d): %s" % (
-            self.sender_id.nickname, 
+            self.sender_id.nickname,
             self.conversation_id.id,
             self.text
         )
+#
+# class Admin(models.Model):
+#     admin = models.ManyToManyField(User)
+# class Group(models.Model):
+#     group_name = models.CharField(max_length = 200)
+#     admin = models.ManyToManyField(Admin)
+#     members = models.ManyToManyField(User,default = None,blank = True)
+#
+#     def __str__(self):
+#         return f"{self.id}"
+#
+# class GroupMessage(models.Model):
+#     sender_id = models.ForeignKey(
+#         User,
+#         on_delete=models.CASCADE)
+#     conversation_id = models.ForeignKey(
+#         Group,
+#         on_delete=models.CASCADE
+#     )
+#     text = models.TextField()
+#     # date = models.DateTimeField(("time"), auto_now=False, auto_now_add=False)
+#     # is_seen = models.BooleanField(("is_seen"),default=False)
+#
+#     def __str__(self):
+#         return "%s (%d): %s" % (
+#             self.sender_id.nickname,
+#             self.conversation_id.id,
+#             self.text
+#         )
