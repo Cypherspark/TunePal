bfe46c07957a199c33f68ea93c0abe55a0705998
reza <rezamansourikhah@gmail.com>
2020-05-28 18:10:30 +0430

    adda url

--- chat/api/urls.py
+++ chat/api/urls.py
@@ -3,8 +3,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^lastimage/$',
+        views.LastAvatar.as_view(),
+        name='lastimage'),
     path('', views.simple_chat, name='conv'),
     url(r'^inbox/$',views.all_inboxes,name='new-messages'),
     re_path('(?P<userparameter>\d{0,100}/$)', views.simple_chat, name='messages'),
+
     # path('inboxes', views.inboxes, name="inboxes")
 ]
