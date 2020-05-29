a5e37df0f2c8daacd054a87edb9db4873789a944
Your Name <you@example.com>
2020-05-30 03:33:16 +0430

    debug avatar serializer

--- account/api/serializers.py
+++ account/api/serializers.py
@@ -13,7 +13,7 @@ def calculateAge(birthDate):
 
 
 class SetUserAvatarSerializer(serializers.ModelSerializer):
-    image = serializers.ImageField(allow_empty_file=False)
+    image = serializers.ImageField()
     class Meta:
         model = Avatar
         fields = ["image"]
