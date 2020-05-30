e50318636150f4ef3ace5ca97755073feeb5fca3
reza <rezamansourikhah@gmail.com>
2020-05-30 19:07:55 +0430

    add a url

--- chat/api/urls.py
+++ chat/api/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    
     url(r'^friendinfo/$',views.User_Friend_Info,name='User_Friend_Info'),
     path('', views.simple_chat, name='conv'),
     url(r'^inbox/$',views.all_inboxes,name='new-messages'),
