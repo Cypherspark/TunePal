bafdd7f25af30148fbfe08af9ac3c5fd430d2ab8
Your Name <you@example.com>
2020-05-31 20:06:43 +0430

    fix pending bub

--- music/api/serializers.py
+++ music/api/serializers.py
@@ -91,7 +91,7 @@ class UserInfoSerializer1(serializers.ModelSerializer):
 
     def get_pendding(self, obj):
         try:
-            relation = FriendshipRequest.objects.filter(to_user=obj, from_user=self.context['request'].user)
+            relation = FriendshipRequest.objects.filter(to_user=obj, from_user=self.context['request'].user)[0]
             status = True
         except:
             status = False
