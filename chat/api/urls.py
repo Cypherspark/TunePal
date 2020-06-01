09abad19fa3badc3754fe6d88996ec09d0189df4
reza <rezamansourikhah@gmail.com>
2020-06-01 19:10:10 +0430

    add addmember url

--- chat/api/urls.py
+++ chat/api/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     url(r'^makegroup/$',views.Make_Group,name="makegroup"),
-    url(r'^groupinfo/$',views.User_Groups,name='group_info'),
-    url(r'^usergroup/$',views.User_Groups,name='usergroup'),
+    url(r'^groupinfo/$',views.Info_Groups,name='group_info'),
+    url(r'^groupmember/$',views.Group_member,name='groupmember'),
+    url(r'^addmember/$',views.Add_Member.as_view(),name='addmember'),
     url(r'^friendinfo/$',views.User_Friend_Info,name='User_Friend_Info'),
     path('', views.simple_chat, name='conv'),
     url(r'^inbox/$',views.all_inboxes,name='new-messages'),
