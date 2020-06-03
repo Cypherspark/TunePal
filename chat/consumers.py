885de8fbac0a656bbd80fe57f2a8fd60d98f7899
Cypherspark <persianrobo4@gmail.com>
2020-06-03 05:10:13 +0430

    fix debug print

--- chat/consumers.py
+++ chat/consumers.py
@@ -24,12 +24,13 @@ class ChatConsumer(WebsocketConsumer):
     def connect(self):
         
         # self.user = self.scope["user"]
-        print("-------> i'm ",self.user)
-        print(self.scope["headers"])
+        # print("-------> i'm ",self.user)
+        # print(self.scope["headers"])
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         user_id = self.scope['url_route']['kwargs']['room_name']
         self.user = get_user(user_id)
         user_id = self.user.id 
+        print("-------> i'm ",self.user)
         # self.room_group_name = 'chat_%s' % self.room_name
         self.room_group_name =  "{}".format(user_id)
         # Join room group
