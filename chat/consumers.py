f662192074ac25ddd9266f6a0ce284911d451341
Masoud Golestaneh <52936110+Cypherspark@users.noreply.github.com>
2020-06-03 19:10:08 +0430

    Hotfix/websocket (#34)
    * change websocket
    
    * fix debug print
    
    * debug new socket
    
    * debug urls
    
    * debug socket
    
    * debug socket
    
    * debug socket
    
    * sync to async
    
    * debug socket
    
    * debug chat
    
    * debug socket
    
    * debug send user
    
    * debug user in socket
    
    * debug message model
    
    * debug model message socket methods
    
    * debug message model socket methods
    
    * debug message model socket
    
    * debug chat consumer
    
    * fix typo
    
    * debug consumer
    
    * fix consumer
    
    * add debug print
    
    * fix number of conversastions for a group
    
    * chnage to default auth socket
    
    * fix self.room  error ref before assignment
    
    * change to debug mode
    
    * add group info to socket
    
    * fix typo
    
    * fix var name
    
    * fix syntax error in consumer
    
    Co-authored-by: Cypherspark <persianrobo4@gmail.com>

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,83 +1,117 @@
 import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 from chat.models import Conversation,Message
 from account.models import CustomUser as User
 from chat.api.serializers import MessageSerializer
 from rest_framework import serializers
 from channels.db import database_sync_to_async
 from datetime import datetime
+ 
 
 @database_sync_to_async
 def make_seen(message_ID):
-    messageObject = Message.objects.get(message_ID)
+    messageObject = Message.objects.get(id = message_ID)
     messageObject.is_seen = True
     messageObject.save()
 
-class ChatConsumer(WebsocketConsumer):
-    
-    def connect(self):
-        
-        self.user = self.scope["user"]
-        print("-------> i'm ",self.user)
-        print(self.scope["headers"])
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
+@database_sync_to_async
+def get_user(userName):
+    return User.objects.get(username = userName)
 
-        # Join room group
-        async_to_sync(self.channel_layer.group_add)(
+@database_sync_to_async
+def make_message(user ,message , ID):
+    c = Conversation.objects.get(id = ID)
+    messageObject = Message.objects.create(sender_id = user,conversation_id = c ,text = message,date = datetime.now() )
+    return messageObject.id
+
+@database_sync_to_async
+def GroupInfo(ID):
+    c = Conversation.objects.get(id = ID)
+    flag = c.is_group
+    name = c.name
+    return [flag, name]
+
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    
+    async def connect(self):
+        
+        # self.user = self.scope["user"]
+        # print("-------> i'm ",self.user)
+        # print(self.scope["headers"])
+        # self.room_name = self.scope['url_route']['kwargs']['room_name']
+        user_id = self.scope['url_route']['kwargs']['room_name']
+        self.user = await get_user(user_id)
+        # print("----ddddddd---> i'm ", user_id)
+        user_id = self.user.id 
+        print("-------> i'm ", self.user)
+        # self.room_group_name = 'chat_%s' % self.room_name
+        self.room_group_name =  "{}".format(user_id)
+        # Join room groupis_group
+        await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
 
 
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
+        await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
     # Receive message from WebSocket
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['text']
-        c = Conversation.objects.get(id = self.room_name)
-        messageObject = Message.objects.create(sender_id =self.user,conversation_id = c ,text = message,date = datetime.now() )
+        ID = text_data_json['id']
+
+        messageID = await make_message(self.user , message, ID)
         # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
-            {
-                "messageID" : messageObject.id,
-                "date" : f"{datetime.now()}",
-                'type': 'chat_message',
-                'message': message,
-                'username': self.user.username,
-                'nickame': self.user.nickname
+        # await self.channel_layer.group_send(
+        #     self.room_group_name,
+        #     {
+        #         "messageID" : messageID,
+        #         "date" : f"",
+        #         'type': 'chat_message',
+        #         'message': message,
+        #         'username': self.user.username,
+        #         'nickame': self.user.nickname,
+        #         'conversation_id': ID
                 
-            }
-        )
+        #     }
+        # )
         
 
   
 
     # Receive message from room group
-    def chat_message(self, event):
+    async def chat_message(self, event):
         message = event['message']
         date1 = event['date']
         date = date1.replace(" ", "T") + "Z"
         username = event['username']
         nickname = event['nickame']
+        conversation_id = event['conversation_id']
+
         is_me = (username == self.user.username)
         if not is_me:
-            make_seen(int(event['messageID']))
+            await make_seen(int(event['messageID']))
+        info = await GroupInfo(int(conversation_id))
+        is_group = info[0]
+        name = info[1]
         # Send message to WebSocket
-        self.send(text_data=json.dumps({
-            "conversation_id": self.room_name,
+        await self.send(text_data=json.dumps({
+            "group_name" : name,
+            "is_group": is_group,
             "date" : date,
             'is_client': is_me,
             "sender_id" : {"nickname":nickname} ,
-            "text": message,            
+            "text": message,
+            "conversation_id":conversation_id           
         }))    
--- chat/models.py
+++ chat/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from account.models import CustomUser as User
 from django.utils.translation import ugettext_lazy as _
+from datetime import datetime
+
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
+
 
 # Create your models here.
 
@@ -25,6 +30,41 @@ class Message(models.Model):
     text = models.TextField()
     date = models.DateTimeField(_("time"), auto_now=False, auto_now_add=False)
     is_seen = models.BooleanField(_("is_seen"),default=False)
+
+    def notify_ws_clients(self):
+        """
+        Inform client there is a new message.
+        """
+        notification = {
+            'type': 'chat_message',
+            'message': self.text,
+            "date" : f"{datetime.now()}",
+            "messageID" : self.id,
+            'conversation_id': self.conversation_id.id,
+            'username': self.sender_id.username,
+            'nickame': self.sender_id.nickname         
+        }
+
+        channel_layer = get_channel_layer()
+        # print("user.id {}".format(self.sender_id.id))
+        # print("user.id {}".format(self.conversation_id.id))
+
+        async_to_sync(channel_layer.group_send)("{}".format(self.sender_id.id), notification)
+        for person in self.conversation_id.members.exclude(id = self.sender_id.id).values_list('id', flat=True):
+            print("person: " ,person)
+            async_to_sync(channel_layer.group_send)("{}".format(person), notification)
+
+    def save(self, *args, **kwargs):
+        """
+        Trims white spaces, saves the message and notifies the recipient via WS
+        if the message is new.
+        """
+        new = self.id
+        self.text = self.text.strip()  # Trimming whitespaces from the body
+        super(Message, self).save(*args, **kwargs)
+        if new is None:
+            self.notify_ws_clients()
+
 
     def __str__(self):
         return "%s (%d): %s" % (
