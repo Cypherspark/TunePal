d920dc08fbfcd85761e50865bda3e6bc5a54ae92
Masoud Golestaneh <masoud.golestaneh@snapp.cab>
2023-05-22 09:36:46 +0330

    admin register models

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
+from django.conf import settings
 
+
+settings.auto_register_models('spotipy')
+settings.auto_register_models('account')
+settings.auto_register_models('music')
+settings.auto_register_models('chat')
+settings.auto_register_models('quiz')
 
 # TokenAdmin.raw_id_fields = ['user']
 # models = apps.get_models()
