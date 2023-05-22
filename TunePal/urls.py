992203386118bf6bf9144098cf407d070c514285
Masoud Golestaneh <masoud.golestaneh@snapp.cab>
2023-05-22 08:53:30 +0330

    add admin urls

--- TunePal/urls.py
+++ TunePal/urls.py
@@ -57,7 +57,7 @@ urlpatterns = [
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    # path('admin/', admin.site.urls),
+    path('api/admin/', admin.site.urls),
     path('api/account/', include('account.api.urls')),
     path('api/chat/', include('chat.api.urls')),
     path('api/spotify/', include('music.api.urls')),
