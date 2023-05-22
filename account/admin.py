774cfae611627bde885b36f375bc304b51a55d6a
Masoud Golestaneh <masoud.golestaneh@snapp.cab>
2023-05-22 09:39:51 +0330

    fix registering models

--- account/admin.py
+++ account/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
-from django.conf import settings
+from django.apps import apps
+
+def AUTO_REGISTER(app_name):
+    app_models = apps.get_app_config(app_name).get_models()
+    for model in app_models:
+        try:
+            admin.site.register(model)
+        except admin.sites.AlreadyRegistered:
+            pass
 
 
-settings.AUTO_REGISTER('spotipy')
-settings.AUTO_REGISTER('account')
-settings.AUTO_REGISTER('music')
-settings.AUTO_REGISTER('chat')
-settings.AUTO_REGISTER('quiz')
+AUTO_REGISTER('spotipy')
+AUTO_REGISTER('account')
+AUTO_REGISTER('music')
+AUTO_REGISTER('chat')
+AUTO_REGISTER('quiz')
 
 # TokenAdmin.raw_id_fields = ['user']
 # models = apps.get_models()
